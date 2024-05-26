e8a02f319fea33053f43d173d31dd2855afd766c
visadarknet <visadarknet@gmail.com>
2024-05-26 21:26:53 +0500

    added products

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,16 @@
 $(function () {
 
+  $('.product-tabs__top-item').on('click', function (e) {
+    e.preventDefault();
+    $('.product-tabs__top-item').removeClass('product-tabs__top-item--active');
+    $(this).addClass('product-tabs__top-item--active');
+
+    $('.product-tabs__content-item').removeClass('product-tabs__content-item--active');
+    $($(this).attr('href')).addClass('product-tabs__content-item--active');
+
+  });
+
+
   $('.product-slide__thumb').slick({
     asNavFor: '.product-slide__big',
     focusOnSelect: true,
@@ -7,13 +18,13 @@ $(function () {
     slidesToScroll: 1,
     vertical: true,
     draggable: false
-    
+
   });
   $('.product-slide__big').slick({
     asNavFor: '.product-slide__thumb',
     draggable: false,
     arrows: false,
-    fade:true
+    fade: true
   });
 
   $('.shop-content__filter-btn').on('click', function () {
